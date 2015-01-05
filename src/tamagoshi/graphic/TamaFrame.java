f5791e212a2378f3fc1328bf89f79622c4eaf1d2
monzey <maxi.bertrand@gmail.com>
2015-01-05 14:49:36 +0100

    readme

---
+++ src/tamagoshi/graphic/TamaFrame.java
@@ -0,0 +1,29 @@
+package tamagoshi.graphic;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class TamaFrame extends JFrame {
+
+	public TamaFrame() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TamaFrame(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TamaFrame(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TamaFrame(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
